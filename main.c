6828adf5c3f84f6d26a636c1b3d963179f08c6f3
Danielgb23 <33136385+Danielgb23@users.noreply.github.com>
2019-03-14 16:10:15 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,121 @@
+// Roteiro 2 - Programa 2a - EA871-1S2019 - Aciona LEDs da placa Shield
+#include "derivative.h" /* include peripheral declarations */
+#define SET_BIT(reg, idx)	reg = (reg | (1 << idx))		// Macro que seta o bit idx do registrador reg
+
+/* Inicializa os GPIOs */
+void init_GPIO(void) {
+
+	SET_BIT(SIM_SCGC5, 11);		// Habilita clock do PORTC (System Clock Gating Control)
+
+	PORTC_PCR0 &= 0xFFFFF8FF;	// Configura MUX para PTC0-PTC7 e PTC10 aparecerem nos pinos
+	SET_BIT(PORTC_PCR0, 8);		
+	PORTC_PCR1 &= 0xFFFFF8FF;
+	SET_BIT(PORTC_PCR1, 8);
+	PORTC_PCR2 &= 0xFFFFF8FF;
+	SET_BIT(PORTC_PCR2, 8);
+	PORTC_PCR3 &= 0xFFFFF8FF;
+	SET_BIT(PORTC_PCR3, 8);
+	PORTC_PCR4 &= 0xFFFFF8FF;
+	SET_BIT(PORTC_PCR4, 8);
+	PORTC_PCR5 &= 0xFFFFF8FF;
+	SET_BIT(PORTC_PCR5, 8);
+	PORTC_PCR6 &= 0xFFFFF8FF;
+	SET_BIT(PORTC_PCR6, 8);
+	PORTC_PCR7 &= 0xFFFFF8FF;
+	SET_BIT(PORTC_PCR7, 8);
+	PORTC_PCR10 &= 0xFFFFF8FF;
+	SET_BIT(PORTC_PCR10, 8);
+
+	GPIOC_PDOR = 0;			// Garante nivel 0 em todas as saidas do PORTC
+	GPIOC_PDDR = 0x000004FF;	// Configura bits 0-7 e 10 do PORTC como saidas
+	GPIOC_PSOR = (1<<10);       	// Habilita o LE (Latch Enable) do registrador dos LEDs vermelhos (74HC573)
+	GPIOC_PCOR = (1<<10);		// Desabilita o LE (Latch Enable) do registrador dos LEDs vermelhos (74HC573)
+
+	//parte dos botões
+	SET_BIT(SIM_SCGC5, 9); 		// Habilita clock do PORTA
+
+	PORTA_PCR4 &= 0xFFFFF8FF;	// Configura MUX para PTA4, PTA5, PTA12 aparecerem nos pinos
+	SET_BIT(PORTA_PCR4, 8);
+	PORTA_PCR5 &= 0xFFFFF8FF;
+	SET_BIT(PORTA_PCR5, 8);
+	PORTA_PCR12 &= 0xFFFFF8FF;
+	SET_BIT(PORTA_PCR12, 8);
+
+	GPIOA_PDDR = 0; 		// Configura todos bits do PORTA como entrada
+
+}
+
+void delay(unsigned int tempo) {
+
+	for ( ; tempo != 0; tempo--);
+}
+
+int le_botao(){	//le o botao e retorna 1(esquerda) ,2muda ou 4(direita) para os botoes, se dois botoes forem apertados retorna a soma deles
+
+	uint16_t a=0, pta4=0, pta5=0, pta12=0;
+
+	while( !pta4  || !pta5 || !pta12) { //espera apertar um botao
+		a = GPIOA_PDIR;       // Faz a leitura no PORTA (16 bits)
+		pta4  = a & (1<<4);   
+		pta5  = a & (1<<5);  
+		pta12 = a & (1<<12);  
+	}
+
+	while( pta4  || pta5 || pta12) {//espera todos os botoes serem desapertados
+		a = GPIOA_PDIR;       // Faz a leitura no PORTA (16 bits)
+		pta4  = a & (1<<4);   
+		pta5  = a & (1<<5);  
+		pta12 = a & (1<<12);  
+
+		//calcula valor de a dependendo dos botoes
+		if (pta4)
+			a|=1;
+		if (pta5)
+			a|=2;
+		if (pta12)
+			a|=4;
+	}
+
+	return a;
+}
+
+
+int main(void) {
+
+	init_GPIO();
+
+	uint8_t leds=0, botao;
+
+	for(;;){
+		//le o botao
+		botao=le_botao();
+
+		//muda estado do led D7
+		if ( botao & 2 )
+			leds ^= 1<<8;
+
+
+		//se os dois botoes de direcao estiverem apertados nao faz nada
+		if ( botao & 1 && botao & 4 )
+			leds+42;
+
+		//desloca leds para esquerda
+		else if ( botao & 1 )
+			leds=leds<<1;
+
+		//desloca leds para a direita
+		else if ( botao & 4 )
+			leds=leds>>1;
+
+
+		//ascende leds
+		GPIOC_PDOR =  leds;		
+
+		GPIOC_PSOR = (1<<10);	//latch enable 74HC573
+		GPIOC_PCOR = (1<<10);
+
+	}
+}
+
+
+
